e969418fa20f38ca8eeb3bd97c32d405e2297495
Mriganka Gogoi <gogoimriganka09@gmail.com>
2023-08-02 12:11:13 +0530

    layout is set

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import hero from "./illustration-hero.svg";
+import Plan from "./Plan";
 function App() {
-  return <main></main>;
+  return (
+    <main className="card">
+      <img className="hero-img" src={hero} alt="hero" />
+      <h2>Order Summary</h2>
+      <p>
+        You can now listen to millions of songs, audiobooks, and podcasts on any
+        device anywhre you like!
+      </p>
+      <Plan />
+      <button>Proceed to Payment</button>
+      <button>Cancel Order</button>
+    </main>
+  );
 }
 
 export default App;
